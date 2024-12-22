e1f8ebc5836afe686cf58581370f8e6a2db7460c
Joshua <horheyinc@gmail.com>
2024-12-22 14:45:06 +0100

    iteration 3

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -2,4 +2,25 @@
 // Iteration 2 | Functions
 //
 
+//2.1
+function calcMultiplication(a: number, b: number): number | string {
+  if (typeof a !== "number" || typeof b !== "number")
+    return "Please provide two numbers";
+  return a * b;
+}
 
+//2.2
+function isEven(num1: number): boolean {
+  return num1 % 2 === 0 ? true : false;
+}
+
+console.log(isEven(2));
+console.log(isEven(3));
+
+//2.3
+function calcArrayAverage(arr: number[]): string | number {
+  if (!arr.length) return "sorry, no numbers in array";
+  return arr.reduce((a: number, c: number) => a + c, 0) / arr.length;
+}
+console.log(calcArrayAverage([1, 2, 3]));
+console.log(calcArrayAverage([]));
--- src/iteration3.ts
+++ src/iteration3.ts
@@ -1,5 +1,37 @@
 //
 // Iteration 3 | Classes
 //
-
-
+class BankAccount {
+  balance: number = 0;
+  accountHolder: string = "";
+  constructor(person: string) {
+    this.accountHolder = person;
+  }
+  getBalance(): number {
+    return this.balance;
+  }
+  deposit(amount: number): string | number {
+    if (amount > 0) {
+      this.balance = this.balance + amount;
+      return this.balance;
+    }
+    return "Pls provide a valid amount";
+  }
+  withdraw(amount: number): string | number {
+    if (amount > 0 && amount < this.balance) {
+      this.balance = this.balance - amount;
+      return this.balance;
+    } else if (amount > 0 && amount > this.balance) {
+      return "Insufficient funds";
+    }
+    return "Pls provide a valid amount";
+  }
+}
+const newAcc = new BankAccount("Ragnar");
+console.log(newAcc);
+console.log(newAcc.getBalance());
+console.log(newAcc.deposit(50));
+console.log(newAcc.deposit(-5));
+console.log(newAcc.withdraw(25));
+console.log(newAcc.withdraw(0));
+console.log(newAcc.withdraw(50));
