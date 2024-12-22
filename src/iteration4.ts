dd6345f962d33e53cf4a03682bd551d24ca958ee
Joshua George <horheyinc8@gmail.com>
2024-12-22 23:50:11 +0100

    all iterations

--- src/iteration4.ts
+++ src/iteration4.ts
@@ -28,3 +28,25 @@ type MusicAlbum = {
   durationInMinutes: number;
   albumType: "Studio Album" | "Live Album" | "Soundtrack";
 };
+
+const ourBook: Book = {
+  title: "Ragnars Trip",
+  year: 2021,
+  genres: ["action", "suspense"],
+  rating: 5,
+  author: "Joshua",
+  numberOfPages: 222,
+};
+const ourMovie: Movie = {
+  title: "Ragnars Movie Trip",
+  year: 2022,
+  genres: ["action", "suspense"],
+  rating: 5,
+  director: "Joshua",
+  durationInMinutes: 90,
+};
+function getYearsSinceRelease(obj: Movie | MusicAlbum | Book): number {
+  return 2024 - obj.year;
+}
+console.log(getYearsSinceRelease(ourBook));
+console.log(getYearsSinceRelease(ourMovie));
--- src/iteration5.ts
+++ src/iteration5.ts
@@ -1,5 +1,27 @@
 //
 // Bonus: Iteration 5 | Interfaces
 //
+interface MediaItem {
+  title: string;
+  year: number;
+  genres: string[];
+  rating: number;
+}
 
-
+interface Book extends MediaItem {
+  author: string;
+  numberOfPages?: number;
+}
+interface Movie extends MediaItem {
+  director: string;
+  durationInMinutes?: number;
+}
+interface MusicAlbum extends MediaItem {
+  artist: string;
+  numberOfTracks: number;
+  durationInMinutes?: number;
+  albumType: "Studio Album" | "Live Album" | "Soundtrack" | "Other";
+}
+function getYearsSinceRelease(obj: MediaItem): number {
+  return 2025 - obj.year;
+}
